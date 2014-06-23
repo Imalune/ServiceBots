9739f229c75552f7f6a0bd4e32382d49e144c165
Imalune <devimalune@gmail.com>
2014-06-23 11:09:16 -0700

    Adding Recipes
    Signed-off-by: Imalune <devimalune@gmail.com>

--- src/main/java/servicebots/ServiceBotsClass.java
+++ src/main/java/servicebots/ServiceBotsClass.java
@@ -2,6 +2,12 @@ package servicebots;
 
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import servicebots.block.BotBlocks;
+import sun.org.mozilla.javascript.internal.ast.Block;
 
 /**
  * Created by Imalune on 6/22/2014.
@@ -13,6 +19,19 @@ public class ServiceBotsClass {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event)
     {
-        BotBlocks.init()
+        BotBlocks.init();
+        GameRegistry.addShapedRecipe(new ItemStack(BotBlocks.blockBotSide),
+                "BBB","ICI","BBB",'B', BotBlocks.itemBlockSteel,'I',BotBlocks.itemIronChunk,'C',BotBlocks.itemConsole);
+        GameRegistry.addShapedRecipe(new ItemStack(BotBlocks.blockBotStation),
+                "B B"," C ","   ",'B',BotBlocks.itemBlockSteel,'C',Blocks.crafting_table);
+        GameRegistry.addShapedRecipe(new ItemStack(BotBlocks.itemBlockSteel),
+                "DDD","DID","DDD",'D', Blocks.dirt,'I',BotBlocks.itemIronChunk);
+        GameRegistry.addShapedRecipe(new ItemStack(BotBlocks.blockBotSteelGenerator),
+                "BHB","SRS","BHB",'B',BotBlocks.itemBlockSteel,'H',BotBlocks.itemHoloClip,'S', Blocks.stone,'R',Items.redstone);
+        GameRegistry.addShapedRecipe(new ItemStack(BotBlocks.blockChargeBlock),
+                "SPS","TGT","TTT",'S',BotBlocks.itemStarter,'P',BotBlocks.itemSparkStarter,'T',Blocks.stone,'G',Blocks.glass);
+        GameRegistry.addShapedRecipe(new ItemStack(BotBlocks.itemCompactor),
+                "S S","CIC","TTT",'S',BotBlocks.itemStarter,'C',Blocks.cobblestone,'I',BotBlocks.itemIronChunk,'T',Blocks.stone);
+
     }
 }
--- src/main/java/servicebots/block/BotBlocks.java
+++ src/main/java/servicebots/block/BotBlocks.java
@@ -1,8 +1,48 @@
 package servicebots.block;
 
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.block.material.Material;
+
 /**
  * Created by Imalune on 6/22/2014.
  */
 public class BotBlocks {
-    public static
+    public static BotSide blockBotSide;
+    public static BotStation blockBotStation;
+    public static BotSteel itemBlockSteel;
+    public static BotSteelGenerator blockBotSteelGenerator;
+    public static ChargeBlock blockChargeBlock;
+    public static Compactor itemCompactor;
+    public static CompactorChip itemCompactorChip;
+    public static Console itemConsole;
+    public static EZBotMover blockEZBotMover;
+    public static FaceConsole blockFaceConsole;
+    public static HoloClip itemHoloClip;
+    public static IronChunk itemIronChunk;
+    public static SparkStarter itemSparkStarter;
+    public static Starter itemStarter;
+
+    public static void init(){
+        blockBotSide = new BotSide(Material.iron);
+        blockBotStation = new BotStation(Material.iron);
+        blockBotSteelGenerator = new BotSteelGenerator(Material.iron);
+        blockChargeBlock = new ChargeBlock(Material.iron);
+        blockEZBotMover = new EZBotMover(Material.iron);
+        blockFaceConsole = new FaceConsole(Material.iron);
+
+        GameRegistry.registerBlock(blockBotSide,"BotSide");
+        GameRegistry.registerBlock(blockBotStation,"BotStation");
+        GameRegistry.registerItem(itemBlockSteel,"BotSteel");
+        GameRegistry.registerBlock(blockBotSteelGenerator,"BotSteelGenerator");
+        GameRegistry.registerBlock(blockChargeBlock,"ChargeBlock");
+        GameRegistry.registerItem(itemCompactor,"Compactor");
+        GameRegistry.registerItem(itemCompactorChip,"CompactorChip");
+        GameRegistry.registerItem(itemConsole,"Console");
+        GameRegistry.registerBlock(blockEZBotMover,"EZBotMover");
+        GameRegistry.registerBlock(blockFaceConsole,"FaceConsole");
+        GameRegistry.registerItem(itemHoloClip,"HoloClip");
+        GameRegistry.registerItem(itemIronChunk,"IronChunk");
+        GameRegistry.registerItem(itemSparkStarter,"SparkStarter");
+        GameRegistry.registerItem(itemStarter,"Starter");
+    }
 }
