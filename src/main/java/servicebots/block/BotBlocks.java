fdad04e042c1c3b26443e8c9683acf3468b531da
Imalune <devimalune@gmail.com>
2014-06-23 15:08:53 -0700

    Fixing entity stuff.
    Signed-off-by: Imalune <devimalune@gmail.com>

--- src/main/java/servicebots/block/BotBlocks.java
+++ src/main/java/servicebots/block/BotBlocks.java
@@ -2,6 +2,7 @@ package servicebots.block;
 
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.material.Material;
+import servicebots.entities.*;
 
 /**
  * Created by Imalune on 6/22/2014.
--- src/main/java/servicebots/block/BotBox.java
+++
@@ -1,7 +0,0 @@
-package servicebots.block;
-
-/**
- * Created by Imalune on 6/23/2014.
- */
-public class BotBox {
-}
