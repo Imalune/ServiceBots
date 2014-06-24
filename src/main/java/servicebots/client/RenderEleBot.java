7bf93192d67b441fc48953c65cadbcc74bf0ed22
Imalune <devimalune@gmail.com>
2014-06-23 17:31:15 -0700

    Added Render Ele-Bot and a Practice texture
    Signed-off-by: Imalune <devimalune@gmail.com>

---
+++ src/main/java/servicebots/client/RenderEleBot.java
@@ -0,0 +1,33 @@
+package servicebots.client;
+
+import net.minecraft.client.model.ModelBiped;
+import net.minecraft.client.renderer.entity.RenderBiped;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.util.ResourceLocation;
+
+/**
+ * Created by Imalune on 6/23/2014.
+ */
+public class RenderEleBot extends RenderBiped{
+    //public final ModelBiped botModel;
+    //public RenderEleBot(ModelBiped model) {
+        //super(model, 0.5f);
+       // botModel = model;
+    //}
+
+   // @Override
+    //protected ResourceLocation getEntityTexture(EntityLiving par1EntityLiving) {
+       // if(par1EntityLiving instanceof EntityEleBot){
+          //  return ((EntityEleBot) par1EntityLiving).texture;
+        //}
+       // return super.getEntityTexture(par1EntityLiving);
+   // }
+
+   // @Override
+   // protected ResourceLocation getEntityTexture(Entity par1Entity) {
+       // if(par1Entity instanceof EntityEleBot){
+        //    return ((EntityEleBot) par1Entity).texture;
+        //}
+       // return super.getEntityTexture(par1Entity);
+   // }
+}
--- src/main/java/servicebots/entities/EntityThing.java
+++ src/main/java/servicebots/entities/EntityThing.java
@@ -2,12 +2,15 @@ package servicebots.entities;
 
 import net.minecraft.entity.EntityCreature;
 import net.minecraft.entity.passive.IAnimals;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
+import servicebots.ServiceBots;
 
 /**
  * Created by Imalune on 6/23/2014.
  */
 public class EntityThing extends EntityCreature implements IAnimals {
+    public final ResourceLocation texture = new ResourceLocation(ServiceBots.MODID, "textures/entities/Practice.png");
     public EntityThing(World world){
         super(world);
         //initial variables
