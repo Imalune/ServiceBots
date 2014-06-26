a63238f7af3f2880eff18716ff07904236b2be2b
Imalune <devimalune@gmail.com>
2014-06-25 20:06:59 -0700

    Work damn you
    Signed-off-by: Imalune <devimalune@gmail.com>

--- src/main/java/servicebots/client/RenderBuilderBot.java
+++ src/main/java/servicebots/client/RenderBuilderBot.java
@@ -20,9 +20,10 @@ public class RenderBuilderBot extends Render {
     private static final ResourceLocation texture =
             new ResourceLocation(ServiceBots.MODID,"textures/entities/builderbot.png");
     @Override
-    public void doRender(Entity par1Entity, double par2, double par3, double par4, float par5, float par6)
+    public void doRender(Entity par1Entity, double x, double y, double z, float pitch, float yaw)
     {
         GL11.glPushMatrix();
+        GL11.glTranslated(x, y, z);
         GL11.glTranslatef(0f, .5f, .25f);
         GL11.glScalef(.45f, -.45f, -.45f);
         bindTexture(texture);
