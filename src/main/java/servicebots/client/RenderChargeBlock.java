bcc6e1218573d801036756b9aa24cdd3d3f21ef5
Imalune <devimalune@gmail.com>
2014-06-26 17:00:27 -0700

    Changes
    Signed-off-by: Imalune <devimalune@gmail.com>

--- src/main/java/servicebots/client/RenderChargeBlock.java
+++ src/main/java/servicebots/client/RenderChargeBlock.java
@@ -4,14 +4,17 @@ import cpw.mods.fml.client.FMLClientHandler;
 import cpw.mods.fml.common.registry.EntityRegistry;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.entity.Entity;
+import net.minecraft.init.Blocks;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.AdvancedModelLoader;
 import net.minecraftforge.client.model.IModelCustom;
 import org.lwjgl.opengl.GL11;
 import servicebots.ServiceBots;
+import servicebots.block.BotBlocks;
 
 /**
  * Created by Imalune on 6/23/2014.
@@ -22,15 +25,16 @@ public class RenderChargeBlock extends TileEntitySpecialRenderer {
             AdvancedModelLoader.loadModel(new ResourceLocation("servicebots:models/chargeBlock.obj"));
     private static final ResourceLocation texture =
             new ResourceLocation(ServiceBots.MODID,"textures/entities/chargeblock.png");
-    FMLClientHandler.instance().getClient().getTextureManager().bindTexture(texture);
     @Override
     public void renderTileEntityAt(TileEntity par1Entity, double x, double y, double z, float pitch)
     {
+        int i = BotBlocks.blockChargeBlock.getLightValue(par1Entity.getWorldObj(), (int)x, (int)y, (int)z);
+        FMLClientHandler.instance().getClient().getTextureManager().bindTexture(texture);
         GL11.glPushMatrix();
         GL11.glTranslated(x, y, z);
         GL11.glTranslatef(0f, 0f, .25f);
         GL11.glScalef(.45f, .45f, .45f);
-        bindTexture(texture);
+        Tessellator.instance.setColorOpaque_F(i, i, i);
         model.renderAll();
         GL11.glPopMatrix();
     }
